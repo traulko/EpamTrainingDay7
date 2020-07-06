dc12bda290b1e79ac9672b0cafb0cb5c064c19df
traulko <traulko03@gmail.com>
2020-07-06 19:22:34 +0300

    Second commit

--- src/com/traulko/locale/main/HamletInternational.java
+++ src/com/traulko/locale/main/HamletInternational.java
@@ -2,22 +2,13 @@ package com.traulko.locale.main;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 
 public class HamletInternational {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 3; i++) {
-            System.out.println("1)Английский\n 2)Белорусский\n 3)Любой");
-            int choice = 0;
-            try {
-                choice = scanner.nextInt();
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
             String country = "";
             String language = "";
-            switch (choice) {
+            switch (i) {
                 case 1:
                     country = "US";
                     language = "en";
@@ -33,6 +24,7 @@ public class HamletInternational {
             System.out.println(str1);
             String str2 = resourceBundle.getString("str2");
             System.out.println(str2);
+            System.out.println();
         }
     }
 }
